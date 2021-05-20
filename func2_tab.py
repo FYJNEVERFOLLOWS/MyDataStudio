7755731ca12fbbbe680f5595e4310ab2166987dc
FYJNEVERFOLLOWS <43714882+FYJNEVERFOLLOWS@users.noreply.github.com>
2021-05-20 09:43:35 +0800

    Update func2_tab.py

--- func2_tab.py
+++ func2_tab.py
@@ -1,5 +1,4 @@
 import subprocess
-import sys
 import time
 
 from PyQt5 import sip
@@ -11,122 +10,12 @@ from PyQt5.QtWidgets import *
 import showSqlResult
 import constant
 
-combolist = ['债股策略估值持仓比对-转债', '债股策略估值持仓比对-股票', '股票策略估值持仓比对', '期权策略估值持仓比对', '股指策略估值持仓比对']
+combolist = constant.FUNC2_COMBOLIST
 exec_combolist = ['main_1', 'main_2']
-cmdlist = [constant.INTERPRETER_PATH +" " + constant.MAIN1_PATH, constant.INTERPRETER_PATH +" " + constant.MAIN2_PATH]
+cmdlist = constant.CMDLIST
 # cmdlist = [r"D:\FYJ\PyQt\QtLearning\venv\Scripts\python.exe D:\FYJ\PyQt\choice_data\test.py", r"D:\FYJ\PyQt\MyDataStudio\venv\Scripts\python.exe D:\FYJ\PyQt\MyDataStudio\test.py"]
 
-sqllist = [
-            "SELECT A1.date, A1.product_id, A1.code, IFNULL(t1.amount,0) as totalAmount, IFNULL(t2.amount,0)  as ftsAmount, IFNULL(t3.amount,0) as marginAmount, "
-            "(IFNULL(t1.amount,0) - IFNULL(t2.amount, 0) - IFNULL(t3.amount, 0)) as DIFAmount " 
-            "FROM "
-            "(SELECT product_id, code, date FROM pos_equity WHERE date='{0}' and product_id in ('FH1', 'HT1', 'HT3', 'HT7', 'LH1', 'LH1', 'ZS1') "
-            "UNION "
-            "SELECT product_id, code, date FROM stgy_pos_futures WHERE date='{0}' and code not LIKE 'I%' "
-            "UNION "
-            "SELECT product_id, cbond_code as code, date FROM stgy_pos_margin WHERE date='{0}' "
-            ") as A1 "
-            "LEFT JOIN "
-            "(SELECT product_id, code, sum(quantity) as amount, date from pos_equity where date='{0}' GROUP BY product_id, code ) AS t1 "
-            "ON (t1.product_id, t1.code) = (A1.product_id, A1.code) "
-            "LEFT JOIN "
-            "(SELECT product_id, code, amount, date from stgy_pos_futures WHERE date='{0}') as t2 "
-            "ON (t2.product_id, t2.code) = (A1.product_id, A1.code) "
-            "LEFT JOIN "
-            "(SELECT product_id, cbond_code as code, cbond_amount as amount, date from stgy_pos_margin WHERE date='{0}') as t3 "
-            "ON (t3.product_id, t3.code) = (A1.product_id, A1.code) "
-            "WHERE A1.code not LIKE '5%' "
-            "order by product_id, code ",
-            "SELECT  A1.date, A1.product_id, A1.code, IFNULL(t1.amount,0) as totalAmount, IFNULL(t2.amount,0) as marginAmount, (IFNULL(t1.amount,0) - IFNULL(t2.amount,0)) as DIFAmount FROM "
-            "(SELECT product_id, code, date FROM pos_margin WHERE date='{0}' and product_id  NOT IN('FH2', 'FH3', 'FH9', 'FH10', 'HT2') "
-            "UNION "
-            "SELECT product_id, stock_code as code, date FROM stgy_pos_margin WHERE date='{0}' "
-            ") as A1 "            
-            "LEFT JOIN "
-            "(SELECT date, product_id, code, sum(sell_margin_quantity) as amount FROM pos_margin WHERE date='{0}' GROUP BY product_id, code, date) as t1 "
-            "ON (t1.product_id, t1.code) = (A1.product_id, A1.code) "
-            "LEFT JOIN "
-            "(SELECT date, product_id, stock_code as code, IFNULL(-stock_amount,0) as amount FROM stgy_pos_margin WHERE date='{0}') as t2 "
-            "on (t2.product_id, t2.code) = (A1.product_id, A1.code) ",
-            "SELECT A1.date, A1.product_id, A1.code, IFNULL(t1.amount,0) as totalAmount, IFNULL(t2.amount,0) as stgyAmount, (IFNULL(t1.amount,0) - IFNULL(t2.amount,0)) as DIFAmount "
-            "FROM "            
-            "(SELECT product_id, code, date FROM pos_equity where date='{0}' and product_id in ('FH2', 'FH3', 'FH9', 'FH10', 'HT2') "
-            "UNION "
-            "SELECT product_id, code, date from stgy_pos_stock WHERE date='{0}' "
-            ") AS A1 "            
-            "LEFT JOIN "
-            "(SELECT product_id, code, sum(quantity) as amount, date from pos_equity where date='{0}' AND product_id in ('FH2', 'FH3', 'FH9', 'FH10', 'HT2') GROUP BY product_id, code) AS t1 "
-            "ON (t1.product_id, t1.code) = (A1.product_id, A1.code) "
-            "LEFT JOIN "
-            "(SELECT product_id, code, SUM(amount) as amount, date from stgy_pos_stock WHERE date='{0}' GROUP BY product_id, code) AS t2 "
-            "ON (t2.product_id, t2.code) = (A1.product_id, A1.code) "            
-            "WHERE A1.code not LIKE '5%' "
-            "ORDER BY product_id, code ",
-    
-            "SELECT A1.date, A1.product_id, A1.code, A1.side, IFNULL(t1.amount,0) as total_amount, "
-            "IFNULL(t2.amount,0) as high_amount, IFNULL(t3.amount,0) as lowLongAmount, IFNULL(t4.amount,0) as lowShortAmount, (IFNULL(t1.amount,0) - IFNULL(t2.amount,0) - IFNULL(t3.amount,0) -IFNULL(t4.amount,0)) as DIFAmount, "
-            "IFNULL(t5.amount,0) as tradeAmount, t5.pos_effect, IFNULL(t6.amount,0) as sigAmount "
-            "FROM "
-            "(SELECT product_id, code, side, date FROM pos_option where date='{0}' "
-            "UNION "
-            "SELECT product_id, code, CAST('SELL' AS CHAR) as side, date from stgy_pos_option1 WHERE date='{0}' "
-            "UNION "
-            "SELECT product_id, code, CAST('BUY' AS CHAR) as side, date from stgy_pos_option2 WHERE date='{0}' "
-            "UNION "
-            "SELECT product_id, code, CAST('SELL' AS CHAR) as side, date from stgy_pos_option3 WHERE date='{0}' "
-            "UNION "
-            "SELECT product_id, code, direction as side, date from trade_option WHERE date='{0}' "
-            ") "
-            "AS A1 "
-            "LEFT JOIN "
-            "(SELECT product_id, code, sum(quantity) as amount, side, date from pos_option where date='{0}' GROUP BY product_id, code, side) AS t1 "
-            "ON (t1.product_id, t1.code, t1.side) = (A1.product_id, A1.code, A1.side) "
-            "LEFT JOIN "
-            "(SELECT product_id, code, ABS(amount) as amount, CAST('BUY' AS CHAR) as side, date from stgy_pos_option1 WHERE date='{0}') AS t2 "
-            "ON (t2.product_id, t2.code, t2.side) = (A1.product_id, A1.code, A1.side) "
-            "LEFT JOIN "
-            "(SELECT product_id, code, ABS(amount) as amount, CAST('BUY' AS CHAR) as side, date from stgy_pos_option2 WHERE date='{0}') as t3 "
-            "ON (t3.product_id, t3.code, t3.side) = (A1.product_id, A1.code, A1.side) "
-            "LEFT JOIN "
-            "(SELECT product_id, code, ABS(amount) as amount, CAST('SELL' AS CHAR) as side, date from stgy_pos_option3 WHERE date='{0}') as t4 "
-            "ON (t4.product_id, t4.code, t4.side) = (A1.product_id, A1.code, A1.side) "
-            "LEFT JOIN "
-            "(SELECT date, product_id, code, direction as side, pos_effect, SUM(total_amount) as amount FROM "
-            "(SELECT date, product_id, code, symbol, direction, pos_effect, total_volume as total_amount, avg_price, trans_type, CAST('option' AS CHAR) as source FROM trade_option WHERE date='{0}' "
-            "ORDER BY date, product_id, code) AS trade_union GROUP BY product_id, code, direction) as t5 "
-            "on (t5.product_id, t5.code, t5.side) = (A1.product_id, A1.code, A1.side) "
-            "LEFT JOIN "
-            "(SELECT product_id, code, ABS(SUM(amount)) as amount, side, date FROM "
-                "("
-                "SELECT product_id, code, amount, date, CAST(IF(amount < 0, 'SELL', 'BUY') AS CHAR) as side FROM sig_option1 WHERE date='{0}' "
-                "UNION "
-                "SELECT product_id, code, amount, date, CAST(IF(amount < 0, 'SELL', 'BUY') AS CHAR) as side FROM sig_option2 WHERE date='{0}' "
-                "UNION "
-                "SELECT product_id, code, amount, date, CAST(IF(amount < 0, 'SELL', 'BUY') AS CHAR) as side FROM sig_option3 WHERE date='{0}' "
-                ") as T GROUP BY product_id, date, code, side) as t6 "
-            "ON (t6.product_id, t6.code, t6.side) = (A1.product_id, A1.code, A1.side) "
-            "ORDER BY product_id, code, side ",
-            "SELECT A1.date, A1.product_id, A1.code, A1.side, IFNULL(t1.quantity,0) as totalAmount, ABS(IFNULL(t2.quantity,0)) as stgyAmount, (ABS(IFNULL(t1.quantity,0)) - ABS(IFNULL(t2.quantity,0))) as DIFAmount "
-            "FROM "            
-            "("
-            "SELECT product_id, code, side, date FROM stgy_pos_sidxfts WHERE date='{0}' "
-            "UNION "
-            "SELECT product_id, code, CAST('BUY' AS CHAR) as side, date from pos_sfutures WHERE date='{0}' AND exchange = 'CFFEX' AND buy_quantity > 0 "
-            "UNION "
-            "SELECT product_id, code, CAST('SELL' AS CHAR) as side, date from pos_sfutures WHERE date='{0}' AND exchange = 'CFFEX' AND sell_quantity > 0 "
-            ") "
-            "AS A1 "            
-            "LEFT JOIN "           
-            "(SELECT product_id, code, buy_quantity as quantity, CAST('BUY' AS CHAR) as side, date from pos_sfutures where date='{0}' AND exchange = 'CFFEX' AND buy_quantity > 0 "
-            "UNION "
-            "SELECT product_id, code, sell_quantity as quantity, CAST('SELL' AS CHAR) as side, date from pos_sfutures where date='{0}' AND exchange = 'CFFEX' AND sell_quantity > 0) "
-            "AS t1 "
-            "ON (t1.product_id, t1.code, t1.side) = (A1.product_id, A1.code, A1.side) "
-            "LEFT JOIN "
-            "(SELECT product_id, code, amount as quantity, side, date from stgy_pos_sidxfts WHERE date='{0}') as t2 "
-            "ON (t2.product_id, t2.code, t2.side) = (A1.product_id, A1.code, A1.side) "            
-            "ORDER BY product_id, code, side"
-           ]
+sqllist = constant.SQLLIST
 
 
 class MyThread(QThread):
@@ -141,7 +30,6 @@ class MyThread(QThread):
         self.signalForText.emit(str(text))  # 发射信号
 
     def run(self):
-        # p = subprocess.Popen(r"D:\Git_tasks\task_new\azamsServer\venv\Scripts\python.exe D:\Git_tasks\task_new\azamsServer\main_2.py", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         # p = subprocess.Popen(r"D:\FYJ\PyQt\QtLearning\venv\Scripts\python.exe D:\FYJ\PyQt\choice_data\test.py", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         p = subprocess.Popen(cmdlist[self.param], stdout=subprocess.PIPE, stderr=subprocess.PIPE) # 通过成员变量传参
         while True:
