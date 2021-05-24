05f9788969ba500feded59216cf4bdf81b4c6b9d
FYJNEVERFOLLOWS <43714882+FYJNEVERFOLLOWS@users.noreply.github.com>
2021-05-24 13:25:25 +0800

    Update constant.py

--- constant.py
+++ constant.py
@@ -7,9 +7,13 @@ MYSQL_HOST = "your mysql host path"
 MYSQL_USER = "your username"
 MYSQL_PASSWD = "azReader"
 
-
-MAIN1_PATH = os.path.join(PROJECT_PATH, 'main_1.py')
-MAIN2_PATH = os.path.join(PROJECT_PATH, 'main_2.py')
+MAIN_1_PATH = os.path.join(PROJECT_PATH, 'main_1.py')
+MAIN_2_PATH = os.path.join(PROJECT_PATH, 'main_2.py')
+MAIN_SIG_1_PATH = os.path.join(PROJECT_PATH, 'main_sig_1.py')
+MAIN_SIG_2_PATH = os.path.join(PROJECT_PATH, 'main_sig_2.py')
+MAIN_SIG_3_PATH = os.path.join(PROJECT_PATH, 'main_sig_3.py')
+MAIN_SIG_4_PATH = os.path.join(PROJECT_PATH, 'main_sig_4.py')
+MAIN_SIG_C_PATH = os.path.join(PROJECT_PATH, 'main_sig_c.py')
 
 COMBOLIST = [u'账户资产信息', u'产品信息', u'账户持仓信息', u'策略指令信息', u'策略净值估值信息', u'账户交易信息', u'账户交易流水信息']
 COMBODICT = {0: [u'权益历史信息表', u'期货账户历史信息表', u'两融账户历史信息表', u'期权账户历史信息表'],
@@ -125,8 +129,8 @@ SQLDICT = {0: {0: 'acc_equity', 1: 'acc_futures', 2: 'acc_margin', 3: 'acc_optio
            5: {0: 'trade_all', 1: 'trade_equity', 2: 'trade_futures', 3: 'trade_margin', 4: 'trade_option'},
            6: {0: 'trans_equity', 1: 'trans_futures', 2: 'trans_margin', 3: 'trans_option'}}
 
-FUNC2_COMBOLIST = ['债股策略估值持仓比对-转债', '债股策略估值持仓比对-股票', '股票策略估值持仓比对', '期权策略估值持仓比对', '股指策略估值持仓比对', '导入交易流水-指令', '生成产品净值-指令']
-CMDLIST = [INTERPRETER_PATH + " " + MAIN1_PATH, INTERPRETER_PATH + " " + MAIN2_PATH]
+FUNC2_COMBOLIST = ['债股策略估值持仓比对-转债', '债股策略估值持仓比对-股票', '股票策略估值持仓比对', '期权策略估值持仓比对', '股指策略估值持仓比对', '导入交易流水-指令', '生成产品净值-指令', '债股策略指令生成', '股票策略指令生成', '期权策略指令生成', '股指策略指令生成', '策略指令校验']
+CMDLIST = [INTERPRETER_PATH + " " + MAIN_1_PATH, INTERPRETER_PATH + " " + MAIN_2_PATH, INTERPRETER_PATH + " " + MAIN_SIG_1_PATH, INTERPRETER_PATH + " " + MAIN_SIG_2_PATH, INTERPRETER_PATH + " " + MAIN_SIG_3_PATH, INTERPRETER_PATH + " " + MAIN_SIG_4_PATH, INTERPRETER_PATH + " " + MAIN_SIG_C_PATH]
 
 SQLLIST = [
     "SELECT A1.date, A1.product_id, A1.code, IFNULL(t1.amount,0) as totalAmount, IFNULL(t2.amount,0)  as ftsAmount, IFNULL(t3.amount,0) as marginAmount, "
